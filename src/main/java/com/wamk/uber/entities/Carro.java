5e28d2626dfa070b583adb81865e6297f990c84b
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-09-06 19:24:10 -0300

    Classe Carro

---
+++ src/main/java/com/wamk/uber/entities/Carro.java
@@ -0,0 +1,65 @@
+package com.wamk.uber.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Carro implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String modelo;
+	private Integer ano;
+	private String placa;
+	
+	public Carro() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carro other = (Carro) obj;
+		return Objects.equals(id, other.id);
+	}
+}
