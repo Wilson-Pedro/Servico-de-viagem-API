3a97e9716c252096c4fa9a20ea56f9d87a0f8fdc
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-09-06 19:04:58 -0300

    first commit

---
+++ src/main/java/com/wamk/uber/UberApplication.java
@@ -0,0 +1,13 @@
+package com.wamk.uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wamk/uber/UberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wamk.uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
