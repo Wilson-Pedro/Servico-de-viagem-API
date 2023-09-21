de86a1a8d9bbb8362466d2931d1cdce53cf36828
vsantos1 <vinicius.shyn2@gmail.com>
2023-09-21 00:02:34 -0300

    refactor: injeção de dependências via construtor e remoção de validação desnecessária

--- src/main/java/com/wamk/uber/services/UsuarioService.java
+++ src/main/java/com/wamk/uber/services/UsuarioService.java
@@ -1,56 +1,55 @@
 package com.wamk.uber.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.wamk.uber.dtos.UsuarioDTO;
 import com.wamk.uber.dtos.mapper.UsuarioMapper;
 import com.wamk.uber.entities.Usuario;
 import com.wamk.uber.exceptions.EntidadeNaoEncontradaException;
 import com.wamk.uber.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.validation.Valid;
+import java.util.List;
 
 @Service
 public class UsuarioService {
 
-	@Autowired
-	private UsuarioRepository usuarioRepository;
-	
-	@Autowired
-	private UsuarioMapper usuarioMapper;
-	
-	@Transactional
-	public Usuario save(UsuarioDTO usuarioDTO) {
-		return usuarioRepository.save(usuarioMapper.toEntity(usuarioDTO));
-	}
+    private final UsuarioRepository usuarioRepository;
 
-	public List<Usuario> findAll() {
-		return  usuarioRepository.findAll();
-	}
+    private final UsuarioMapper usuarioMapper;
 
-	public Usuario findById(Long id) {
-		return usuarioRepository.findById(id)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada."));
-	}
+    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioMapper = usuarioMapper;
+    }
 
-	public Usuario atualizarCadastro(@Valid UsuarioDTO usuarioDTO,Long id) {
-		return usuarioRepository.findById(id)
-				.map(usuario -> {
-					usuario.setNome(usuarioDTO.getNome());
-					usuario.setTelefone(usuarioDTO.getTelefone());
-					usuario.setTipoUsuario(usuarioDTO.getTipoUsuario());
-					return usuarioRepository.save(usuario);
-				}).orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada."));
-	}
-	
-	@Transactional
-	public void delete(Long id) {
-		usuarioRepository.delete(usuarioRepository.findById(id)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada.")));
-	}
-	
+    @Transactional
+    public Usuario save(UsuarioDTO usuarioDTO) {
+        return usuarioRepository.save(usuarioMapper.toEntity(usuarioDTO));
+    }
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(Long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada."));
+    }
+
+    public Usuario atualizarCadastro(UsuarioDTO usuarioDTO, Long id) {
+        return usuarioRepository.findById(id)
+                .map(usuario -> {
+                    usuario.setNome(usuarioDTO.getNome());
+                    usuario.setTelefone(usuarioDTO.getTelefone());
+                    usuario.setTipoUsuario(usuarioDTO.getTipoUsuario());
+                    return usuarioRepository.save(usuario);
+                }).orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada."));
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        usuarioRepository.delete(usuarioRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Entidade não encontrada.")));
+    }
+
 }
