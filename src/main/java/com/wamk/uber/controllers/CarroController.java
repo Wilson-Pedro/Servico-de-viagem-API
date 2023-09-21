cdff2b485360226324537ea19fa4790f8a9fab7c
vsantos1 <vinicius.shyn2@gmail.com>
2023-09-20 23:48:25 -0300

    chore: validações desnecessárias e refatorando controller

--- src/main/java/com/wamk/uber/controllers/CarroController.java
+++ src/main/java/com/wamk/uber/controllers/CarroController.java
@@ -1,69 +1,57 @@
 package com.wamk.uber.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.wamk.uber.dtos.CarroDTO;
 import com.wamk.uber.dtos.mapper.CarroMapper;
 import com.wamk.uber.entities.Carro;
 import com.wamk.uber.services.CarroService;
-
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/carros")
-@Validated
 public class CarroController {
 
-	@Autowired
-	private CarroService carroService;
-	
-	@Autowired
-	private CarroMapper carroMapper;
-	
+	private final CarroService carroService;
+
+	private final CarroMapper carroMapper;
+
+	public CarroController(CarroService carroService, CarroMapper carroMapper) {
+		this.carroService = carroService;
+		this.carroMapper = carroMapper;
+	}
+
 	@GetMapping
-	public List<CarroDTO> findAll(){
+	public ResponseEntity<List<CarroDTO>> findAll() {
 		List<Carro> list = carroService.findAll();
-		return list.stream().map(x -> carroMapper.toDTO(x)).toList();
+		return ResponseEntity.status(HttpStatus.OK).body(list.stream().map(carroMapper::toDTO).toList());
 	}
-	
+
 	@GetMapping("/{id}")
-	public CarroDTO findById(@PathVariable Long id){
+	public ResponseEntity<CarroDTO> findById(@PathVariable Long id) {
 		Carro carro = carroService.findById(id);
-		return carroMapper.toDTO(carro);
+		return ResponseEntity.status(HttpStatus.OK).body(carroMapper.toDTO(carro));
 	}
-	
+
 	@PostMapping
-	public CarroDTO registrarCarro(@RequestBody @Valid CarroDTO carroDTO){
+	public ResponseEntity<CarroDTO> registrarCarro(@RequestBody @Valid CarroDTO carroDTO) {
 		var carro = carroService.save(carroDTO);
-		return carroMapper.toDTO(carro);
-		
+		return ResponseEntity.status(HttpStatus.CREATED).body(carroMapper.toDTO(carro));
+
 	}
-	
+
 	@PutMapping("/{id}")
-	public CarroDTO atualiziar(@RequestBody @Valid CarroDTO carroDTO, 
-			@PathVariable @NotNull @Positive Long id){
+	public ResponseEntity<CarroDTO> atualizar(@RequestBody @Valid CarroDTO carroDTO, @PathVariable Long id) {
 		var carro = carroService.atualizarCadastro(carroDTO, id);
-		return carroMapper.toDTO(carro);
+		return ResponseEntity.status(HttpStatus.CREATED).body(carroMapper.toDTO(carro));
 	}
-	
+
 	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteById(@PathVariable @NotNull @Positive Long id){
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
 		carroService.delete(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
