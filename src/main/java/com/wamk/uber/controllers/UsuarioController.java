c50df897977ca9bcd985a93140c374cfae527060
vsantos1 <vinicius.shyn2@gmail.com>
2023-09-21 00:02:15 -0300

    refactor: injeção de dependências via construtor e remoção de anotações desnecessárias

--- src/main/java/com/wamk/uber/controllers/UsuarioController.java
+++ src/main/java/com/wamk/uber/controllers/UsuarioController.java
@@ -1,79 +1,70 @@
 package com.wamk.uber.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.wamk.uber.dtos.SolicitarViagemDTO;
 import com.wamk.uber.dtos.UsuarioDTO;
 import com.wamk.uber.dtos.mapper.UsuarioMapper;
 import com.wamk.uber.entities.Usuario;
 import com.wamk.uber.services.UsuarioService;
 import com.wamk.uber.services.ViagemService;
-
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Validated
+import java.util.List;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-	@Autowired
-	private UsuarioService usuarioService;
-	
-	@Autowired
-	private ViagemService viagemService;
-	
-	@Autowired
-	private UsuarioMapper usuarioMapper;
-	
-	@GetMapping
-	public List<UsuarioDTO> findAll(){
-		List<Usuario> list = usuarioService.findAll();
-		return list.stream().map(x -> usuarioMapper.toDTO(x)).toList();
-	}
-	
-	@GetMapping("/{id}")
-	public UsuarioDTO findById(@PathVariable Long id){
-		var usuario = usuarioService.findById(id);
-		return usuarioMapper.toDTO(usuario);
-	}
-	
-	@PostMapping
-	public UsuarioDTO registrarUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO){
-		var usuario = usuarioService.save(usuarioDTO);
-		return usuarioMapper.toDTO(usuario);
-	}
-	
-	@PutMapping("/{id}")
-	public UsuarioDTO atualiziar(@RequestBody @Valid UsuarioDTO usuarioDTO, 
-			@PathVariable @NotNull @Positive Long id){
-		var usuario = usuarioService.atualizarCadastro(usuarioDTO, id);
-		return usuarioMapper.toDTO(usuario);
-	}
-	
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteById(@PathVariable @NotNull @Positive Long id){
-		usuarioService.delete(id);
-	}
-	
-	@PostMapping("/solicitacao")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void solicitandoViagem(@RequestBody @Valid SolicitarViagemDTO solicitacao) {
-		viagemService.solicitandoViagem(solicitacao);
-	}
+    private final UsuarioService usuarioService;
+
+    private final ViagemService viagemService;
+
+    private final UsuarioMapper usuarioMapper;
+
+    public UsuarioController(UsuarioService usuarioService, ViagemService viagemService, UsuarioMapper usuarioMapper) {
+        this.usuarioService = usuarioService;
+        this.viagemService = viagemService;
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> findAll() {
+        List<Usuario> list = usuarioService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(list.stream().map(usuarioMapper::toDTO).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
+        var usuario = usuarioService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioMapper.toDTO(usuario));
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> registrarUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+        var usuario = usuarioService.save(usuarioDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioMapper.toDTO(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> atualiziar(@RequestBody @Valid UsuarioDTO usuarioDTO,
+                                                 @PathVariable Long id) {
+        var usuario = usuarioService.atualizarCadastro(usuarioDTO, id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioMapper.toDTO(usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable Long id) {
+        usuarioService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping("/solicitacao")
+    public ResponseEntity<Object> solicitandoViagem(@RequestBody @Valid SolicitarViagemDTO solicitacao) {
+        viagemService.solicitandoViagem(solicitacao);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
 }
