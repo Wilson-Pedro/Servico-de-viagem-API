54556b9fd889cdbaa5da6a621378fb10b7048e3a
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-09-29 19:43:34 -0300

    Método 'passageiroOuMotorista()' em UsuarioMapper

--- src/main/java/com/wamk/uber/dtos/mapper/UsuarioMapper.java
+++ src/main/java/com/wamk/uber/dtos/mapper/UsuarioMapper.java
@@ -1,8 +1,11 @@
 package com.wamk.uber.dtos.mapper;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 import com.wamk.uber.dtos.UsuarioDTO;
+import com.wamk.uber.entities.Motorista;
+import com.wamk.uber.entities.Passageiro;
 import com.wamk.uber.entities.Usuario;
 import com.wamk.uber.enums.TipoUsuario;
 import com.wamk.uber.enums.UsuarioStatus;
@@ -29,6 +32,19 @@ public class UsuarioMapper {
 		usuario.setTelefone(usuarioDTO.getTelefone());
 		usuario.setTipoUsuario(TipoUsuario.toEnum(usuarioDTO.getTipoUsuario()));
 		usuario.setUsuarioStatus(UsuarioStatus.ATIVO);
-		return usuario;
+		var user = passageiroOuMotorista(usuario);
+		return user;
+	}
+
+	private Usuario passageiroOuMotorista(Usuario usuario) {
+		if(usuario.getTipoUsuario().equals(TipoUsuario.PASSAGEIRO)) {
+			Passageiro user = new Passageiro();
+			BeanUtils.copyProperties(usuario, user);
+			return user;
+		} else {
+			Motorista user = new Motorista();
+			BeanUtils.copyProperties(usuario, user);
+			return user;
+		}
 	}
 }
