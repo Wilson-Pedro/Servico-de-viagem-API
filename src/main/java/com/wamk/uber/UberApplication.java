cbbd9550beb28a5a45b7e396cbfd36bcad0567b3
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-10-03 16:41:11 -0300

    chore: remoção da entidade SolicitarViagem

--- src/main/java/com/wamk/uber/UberApplication.java
+++ src/main/java/com/wamk/uber/UberApplication.java
@@ -1,6 +1,5 @@
 package com.wamk.uber;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,13 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.wamk.uber.entities.Carro;
 import com.wamk.uber.entities.Motorista;
 import com.wamk.uber.entities.Passageiro;
-import com.wamk.uber.entities.SolicitarViagem;
 import com.wamk.uber.entities.Viagem;
 import com.wamk.uber.enums.FormaDePagamento;
 import com.wamk.uber.enums.TipoUsuario;
 import com.wamk.uber.enums.UsuarioStatus;
 import com.wamk.uber.repositories.CarroRepository;
-import com.wamk.uber.repositories.SolicitarViagemRepository;
 import com.wamk.uber.repositories.UsuarioRepository;
 import com.wamk.uber.repositories.ViagemRepository;
 
@@ -27,23 +24,19 @@ public class UberApplication implements CommandLineRunner{
 	private final UsuarioRepository usuarioRepository;
 
 	private final CarroRepository carroRepository;
-	
-	private final SolicitarViagemRepository solicitarViagemRepository;
 
 	private final ViagemRepository viagemRepository;
 
-	public UberApplication(UsuarioRepository usuarioRepository, CarroRepository carroRepository,
-			SolicitarViagemRepository solicitarViagemRepository, ViagemRepository viagemRepository) {
+	public UberApplication(UsuarioRepository usuarioRepository, CarroRepository carroRepository, ViagemRepository viagemRepository) {
 		this.usuarioRepository = usuarioRepository;
 		this.carroRepository = carroRepository;
-		this.solicitarViagemRepository = solicitarViagemRepository;
 		this.viagemRepository = viagemRepository;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UberApplication.class, args);
 	}
-
+	//SolicitarViagem
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -59,17 +52,12 @@ public class UberApplication implements CommandLineRunner{
 		Motorista m2 = new Motorista(null, "Julia", "9833163865", TipoUsuario.MOTORISTA, UsuarioStatus.ATIVO, c2);
 		Motorista m3 = new Motorista(null, "Julia", "9833163865", TipoUsuario.MOTORISTA, UsuarioStatus.ATIVO, c3);
 		
-		SolicitarViagem sv1 = new SolicitarViagem(null, 
-				"Novo Castelo - Rua das Goiabas 1010", "Pará - Rua das Maçãs", 
-				p1.getNome(), new BigDecimal(18), FormaDePagamento.DEBITO);
-		
 		Viagem v1 = new Viagem(null, 
 				"Novo Castelo - Rua das Goiabas 1010", "Pará - Rua das Maçãs", 
 				"20min", p1,(Motorista) m1, FormaDePagamento.PIX);
 		
 		carroRepository.saveAll(List.of(c1, c2, c3));
 		usuarioRepository.saveAll(Arrays.asList(p1, p2, p3, m1, m2, m3));
-		solicitarViagemRepository.saveAll(List.of(sv1));
 		viagemRepository.saveAll(List.of(v1));
 		
 	}
--- src/main/java/com/wamk/uber/repositories/SolicitarViagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wamk.uber.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wamk.uber.entities.SolicitarViagem;
-
-public interface SolicitarViagemRepository extends JpaRepository<SolicitarViagem, Long>{
-
-}
